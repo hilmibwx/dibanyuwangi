c8852ce577f101f50bf47f6a24740aae4577956e
Hilmi Hidayat <35033991+hilmibwx@users.noreply.github.com>
2021-01-17 19:23:46 +0700

    Update front.blade.php

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -206,6 +206,40 @@
   
 
  </script>
+ 
+ <script language="JavaScript">
+
+// disable view source atau Ctrl+U
+document.onkeydown = function(e) {
+    if(e.keyCode == 123)
+       {
+            return false;
+        }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0))
+       {
+            return false;
+        }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0))
+        {
+             return false;
+         }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))
+         {
+             return false;
+         }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0))
+         {
+             return false;
+         }     
+ }
+
+// disable klik kanan
+document.addEventListener("contextmenu", function(e){
+    e.preventDefault();
+}, false);
+
+</script>
+
 
 
   @stack('scripts')
